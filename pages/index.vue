a279d9d4935df59aa5e7ed612d6f8a8c3ce329e9
Sungkuk Kim <sungkuk5420@gmail.com>
2023-09-24 16:11:13 +0900

    상품 리스트 구현

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,86 @@
 <template>
-  <Tutorial />
+  <div class="app">
+    <main>
+      <div>
+        <input type="text"/>
+      </div>
+      <ul>
+        <li
+          v-for="item in products"
+          :key="item.id"
+          class="item flex"
+          @click="routeToDetailPage(item.id)"
+        >
+          <img class="product-image" :src="item.imageUrl" alt="" />
+          <p>{{ item.name }}</p>
+          <span>{{ item.price }}</span>
+        </li>
+      </ul>
+      <div class="cart-wrapper">
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'IndexPage',
+  async asyncData(){
+    const response = await axios.get('http://localhost:3000/products');
+    console.log(response)
+    const products = response.data
+    return {products}
+  },
+  data(){
+    return {
+    }
+  },
+
+  methods: {
+    routeToDetailPage(id) {
+      this.$router.push(`/product/${id}`)
+    },
+    routeToCartPage() {
+      this.$router.push('/cart')
+    },
+  },
+  async created(){
+  }
+
 }
 </script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
